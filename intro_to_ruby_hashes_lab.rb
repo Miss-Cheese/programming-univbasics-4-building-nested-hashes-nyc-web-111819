60893c9eaf546165e1906617bb9fb6231f1284f0
Natalie Galligan <nat.galligan@gmail.com>
2019-11-06 23:29:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def monopoly_with_third_tier
 
 	abc = monopoly_with_second_tier()
-  abc[:railroads][:rent_in_dollars] = {	:one_piece_owned => "25",
+  abc[:railroads][:rent_in_dollars] = {	:one_piece_owned => 25,
 		:two_pieces_owned => "50",
 		:three_pieces_owned => "100",
 		:four_pieces_owned => "200"}
